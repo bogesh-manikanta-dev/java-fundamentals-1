27a7b91e1124bbb9f3a888fdecd650b0327bb63e
bogesh-manikanta-dev <bogeshmanikanta@gmail.com>
2026-06-07 17:53:52 +0530

    Primitives

---
+++ src/com/java/fundamentals/Primitives.java
@@ -0,0 +1,14 @@
+package com.java.fundamentals;
+
+public class Primitives {
+    static void main() {
+        /*
+        *primitives example
+        * bite variable declaration
+        * */
+        byte age;
+        age=10;
+        System.out.println("AGE:"+ age);
+
+    }
+}
